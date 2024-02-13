d0db5a280eab7bfbc68b0bd93954685db40df50d
Chandu105 <bandaruchandana2001@gmail.com>
2024-02-13 17:27:06 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class School(models.Model):
     sname=models.CharField(max_length=100)
     sprincipal=models.CharField(max_length=100)
     slocation=models.CharField(max_length=100)
-
+    def get_absolute_url(self):
+        return reverse('detail',kwargs={'pk':self.pk})
     def __str__(self):
         return self.sname
 class Student(models.Model):
